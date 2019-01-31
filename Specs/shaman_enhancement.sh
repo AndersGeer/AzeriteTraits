bd76ee3de654d90fd2956fedafac53a801ea4d6a
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-01-31 22:10:46 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,16 +1,13 @@
 wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /t23Enh.txt 
 index=0
 
-cat /t23Enh.txt | while read line; do
+
+
+
+curl -X POST -H "Content-Type: application/json" \
+  -d '{ "apiKey": "1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89", "type": "advanced", "advancedInput": "
+  cat /t23Enh.txt | while read line; do
     echo "$line"
-    index=$(($index+1))
 done
-
-for i in "${MYARRAY[@]}"
-do
-	echo $i
-done
-
-#curl -X POST -H "Content-Type: application/json" \
-#  -d '{ "apiKey": "1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89", "type": "advanced", "advancedInput": ""}' \
-#  https://www.raidbots.com/sim
+"}' \
+  https://www.raidbots.com/sim
