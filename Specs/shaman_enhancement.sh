f053b7c09862f0902a925026257443924773ea19
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-01-31 21:27:10 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,10 +1,12 @@
 wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /t23Enh.txt 
 
-declare -A AArray
-while IFS== read var val ;do
-    [[ "$var" =~ ^[A-Za-z_]*$ ]] && AArray[$var]=$val
-  done </t23Enh.txt
-echo ${AArray[MARX_BUILD_NO]}
+#declare -A AArray
+#while IFS== read var val ;do
+#    [[ "$var" =~ ^[A-Za-z_]*$ ]] && AArray[$var]=$val
+#  done </t23Enh.txt
+#echo ${AArray[MARX_BUILD_NO]}
+
+echo /T23Enh.txt
 
 #curl -X POST -H "Content-Type: application/json" \
 #  -d '{ "apiKey": "blah", "type": "advanced", "advancedInput": "armory=us,malganis,seriallos"}' \
