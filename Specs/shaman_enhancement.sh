5e249ad1b96d8995c0a68c5ef71b915d822fba83
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-01-31 23:06:54 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,7 +1,7 @@
 grep --help > /t23Enh.txt
-echo "'{ "apiKey": "1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89", "type": "advanced", "advancedInput": " > /t23Enh.txt
-wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /t23Enh.txt 
-echo "}'" > /t23Enh.txt
+echo "'{ "apiKey": "1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89", "type": "advanced", "advancedInput": " >> /t23Enh.txt
+wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -A /t23Enh.txt 
+echo "}'" >> /t23Enh.txt
 
 for ln in `cat /t23Enh.txt`; do
     echo "$ln"
