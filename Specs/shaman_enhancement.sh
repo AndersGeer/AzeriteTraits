f5b18293dfaac01b8b217b9326c8efcc3a123f2c
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-01-31 21:49:21 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,7 +1,7 @@
 wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /t23Enh.txt 
 index=0
 cat /t23Enh.txt | while read line; do
-    MYARRAY[$index]="$line\R\N"
+    MYARRAY[$index]="$line"\R\N
     index=$(($index+1))
 done
 
