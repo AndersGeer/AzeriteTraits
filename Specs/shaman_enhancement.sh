d792fee18b26fdfcc039b9852304470cae51dccf
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-01-31 23:26:40 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,12 +1,12 @@
 echo "'{ "apiKey": "1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89", "type": "advanced", "advancedInput": " >> /data.txt
 wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /download.txt
-for ln in `cat /download.txt`; do
-    echo "$ln" >> /data.txt
+cat /download.txt | while read line; do
+    echo "$line" >> /data.txt
 done
 echo "}'" >> /data.txt
 
-for ln in `cat /data.txt`; do
-    echo "$ln"
+cat /data.txt | while read line; do
+    echo "$line" 
 done
 
 #curl -X POST -H "Content-Type: application/json" \
