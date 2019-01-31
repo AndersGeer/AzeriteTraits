f980e4351df193aefa7186c01e46a251dafc8d6b
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-01-31 21:46:25 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -5,10 +5,11 @@ cat /t23Enh.txt | while read line; do
     index=$(($index+1))
 done
 
-curl -X POST -H "Content-Type: application/json" \
-  -d '{ "apiKey": "1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89", "type": "advanced", "advancedInput": 
-"for i in "${array[@]}"
+for i in "${array[@]}"
 do
 	echo $i
-done"}' \
-  https://www.raidbots.com/sim
+done
+
+#curl -X POST -H "Content-Type: application/json" \
+#  -d '{ "apiKey": "1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89", "type": "advanced", "advancedInput": ""}' \
+#  https://www.raidbots.com/sim
