81fa53304e3ee437968172dd4d9c6ea2dc1fb12b
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-07 09:46:16 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,4 +1,4 @@
-echo "{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"advancedInput\": \"" >> /data.txt
+echo "{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"simcVersion\": \"nightly\", \"advancedInput\": \"" >> /data.txt
 wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /download.txt
 cat /download.txt | while read line; do
     echo "$line" | tr "\"" "'" >> /data.txt
@@ -15,7 +15,7 @@ done
 #curl -X POST -H "Content-Type: application/json" -d '{ "apiKey": "blah", "type": "advanced", "advancedInput": "armory=us,malganis,seriallos"}' https://www.raidbots.com/sim
 response=$(curl -X POST -H "Content-Type: application/json" -d @'/data.txt' --write-out %{http_code} --output /dev/null https://www.raidbots.com/sim)
 
-echo "$http_code"
+#echo "$http_code"
 echo "$response"
 
 #curl -X POST -H "Content-Type": "application/json" -d @/data.txt https://www.raidbots.com/sim
