018f0d795665644d0b8b497f99214320a677638c
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-07 10:08:56 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -8,15 +8,14 @@ echo "\"}" >> /data.txt
 
 #echo "{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"advancedInput\": \"armory=eu,draenor,shanyao\"}" > /data.txt
 
-cat /data.txt | while read line; do
-    echo "$line"
-done
+
 #
 #curl -X POST -H "Content-Type: application/json" -d '{ "apiKey": "blah", "type": "advanced", "advancedInput": "armory=us,malganis,seriallos"}' https://www.raidbots.com/sim
-response=$(curl -X POST -H "Content-Type: application/json" -d @'/data.txt' --write-out %{http_code} --output /dev/null https://www.raidbots.com/sim)
+curl -X POST -H "Content-Type: application/json" -d @'/data.txt' --write-out %{http_code} --output /dev/null https://www.raidbots.com/sim >> response.txt
 
-#echo "$http_code"
-echo "$response"
+cat /response.txt | while read line; do
+    echo "$line"
+done
 
 #curl -X POST -H "Content-Type": "application/json" -d @/data.txt https://www.raidbots.com/sim
 #-d "armory=ey,draenor,shanyao"
