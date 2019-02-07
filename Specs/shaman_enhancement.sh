78895f6b1f90430acaf773f4caa4fd93484c35e1
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-07 10:18:12 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -11,13 +11,11 @@ echo "\"}" >> /data.txt
 
 #
 #curl -X POST -H "Content-Type: application/json" -d '{ "apiKey": "blah", "type": "advanced", "advancedInput": "armory=us,malganis,seriallos"}' https://www.raidbots.com/sim
-response=$(curl -X POST -H "Content-Type: application/json" --data-binary @'/data.txt' --write-out %{http_code} --output /dev/null https://www.raidbots.com/sim) >> /response.txt
+response=$(curl -X POST -H "Content-Type: application/json" --data-binary @'/data.txt' --write-out %{http_code} --output /dev/null https://www.raidbots.com/sim)
 
 echo="$response"
 
-cat /response.txt | while read line; do
-    echo "$line"
-done
+
 
 #curl -X POST -H "Content-Type": "application/json" -d @/data.txt https://www.raidbots.com/sim
 #-d "armory=ey,draenor,shanyao"
