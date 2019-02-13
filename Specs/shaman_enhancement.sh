1b95ead6efa64bbb1cec82f11cdb86f808105649
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-13 20:33:08 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,14 +1,19 @@
-F2_world () {
-    retval="\"{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"simcVersion\": \"nightly\", \"advancedInput\":\""
-    wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /download.txt
+Download_Item() {
     worldval=""
     cat /download.txt | while read line; do
     #echo $line
     worldval="$worldval$line \r\n" | tr "\"" "_"
-    #echo $retval
     done
+    echo worldval
+}
+
+F2_world () {
+    retval="\"{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"simcVersion\": \"nightly\", \"advancedInput\":\""
+    wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /download.txt
     
-    retval="$retval $worldval \"}\""
+      
+    
+    retval="$retval $? \"}\""
     
     echo "$retval"
 }
