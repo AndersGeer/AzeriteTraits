18dba8a95d73bfb3459f9fd6be2948067c29ae9b
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-13 20:14:36 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -2,7 +2,9 @@ F2_world () {
     local  retval="\"{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"simcVersion\": \"nightly\", \"advancedInput\": \""
     wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /download.txt
     cat /download.txt | while read line; do
-    retval="$retval $line" | tr "\"" "_"
+    echo $line
+    retval="$retval $line" #| tr "\"" "_"
+    echo $retval
     done
     
     retval="$retval \"}\""
