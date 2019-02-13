dea5cba768386a5bf1d6130a7a99e4b7234a969e
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-13 20:53:32 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -1,21 +1,17 @@
 Download_Item() {
     worldval=""
     cat /download.txt | while read line; do
-    #echo $line
     worldval="$worldval$line \r\n" | tr "\"" "_"
-   
     done
-    echo $worldval
+    echo " --- "
+    return $worldval
 }
 
 F2_world () {
     retval="\"{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"simcVersion\": \"nightly\", \"advancedInput\":\""
     wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /download.txt
-    
     simc=$(Download_Item)
-    
     retval="$retval $simc \"}\""
-    
     echo "$retval"
 }
 
@@ -28,7 +24,7 @@ F2_world () {
 
 
 #echo "{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"advancedInput\": \"armory=eu,draenor,shanyao\"}" > /data.txt
-F2_world
+fullJson=$(F2_world)
 echo $?
 
 #
