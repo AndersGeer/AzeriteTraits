518920dddff1fcece485e2b4b3b3b7e9fd123180
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-13 20:54:50 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -3,7 +3,7 @@ Download_Item() {
     cat /download.txt | while read line; do
     worldval="$worldval$line \r\n" | tr "\"" "_"
     done
-    echo " --- "
+    #echo " --- "
     return $worldval
 }
 
@@ -12,7 +12,7 @@ F2_world () {
     wget "https://raw.githubusercontent.com/simulationcraft/simc/bfa-dev/profiles/Tier23/T23_Shaman_Enhancement.simc" -O /download.txt
     simc=$(Download_Item)
     retval="$retval $simc \"}\""
-    echo "$retval"
+    return "$retval"
 }
 
 #echo "{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"simcVersion\": \"nightly\", \"advancedInput\": \"" >> /data.txt
@@ -25,7 +25,7 @@ F2_world () {
 
 #echo "{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"advancedInput\": \"armory=eu,draenor,shanyao\"}" > /data.txt
 fullJson=$(F2_world)
-echo $?
+echo $fullJson
 
 #
 #curl -X POST -H "Content-Type: application/json" -d '{ "apiKey": "blah", "type": "advanced", "advancedInput": "armory=us,malganis,seriallos"}' https://www.raidbots.com/sim
