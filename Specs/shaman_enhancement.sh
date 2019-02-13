beb45c95591b67e9f37550a16fd2437ff8394b37
Anders Geer Jakobsen <geer.anders@gmail.com>
2019-02-13 19:55:25 +0100

    Update shaman_enhancement.sh

--- Specs/shaman_enhancement.sh
+++ Specs/shaman_enhancement.sh
@@ -23,7 +23,7 @@ echo $?
 #-d "armory=ey,draenor,shanyao"
 
 
-function F2()
+function F2
 {
     local  retval="{\"apiKey\": \"1292e5796497e3a5973a9fa168314af7ca9dfa88f55b4d8d4fb7d7e28bd63a89\", \"type\": \"advanced\", \"simcVersion\": \"nightly\", \"advancedInput\": \""
     
